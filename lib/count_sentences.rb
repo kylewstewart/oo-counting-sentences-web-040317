56108d116d0b76ec6d0db7662fff057773316d6a
kylewstewart <kyle.w.stewart@mac.com>
2017-04-05 17:45:12 -0400

    Completed oo-counting-sentecnes

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    count = 0
+    count_array = self.split(' ')
+    count_array.each do |word|
+      case word[-1]
+      when "."
+        count += 1
+      when "!"
+        count += 1
+      when "?"
+        count += 1
+      end
+    end
+    count
   end
+
 end
